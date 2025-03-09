a45ccfeb135c78dcc78fee3b072fd075e9be8a24
Naga-Umamahesh <umamaheshbatraju@gmail.com>
2025-03-10 01:50:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,6 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "dev" {
-    ami = "ami-00db8dadb36c9815e"
-    instance_type = "t2.nano"
-    tags = {
-      Name = "dev12345678"
-    }
+resource "aws_s3_bucket" "bucket" {
+  bucket = "awfskjgvnf013-123"
 }
